712ba9c5ad120b89467c886d5c93e26d34c4faa3
dfuruya <dfuruya@gmail.com>
2016-05-04 16:27:28 -0700

    Pass SongQueue specs

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,32 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
+    this.on('add', this.enqueue, this);
+    this.on('enqueue', this.enqueue, this);
+    this.on('dequeue', this.dequeue, this);
+    this.on('ended', this.end, this);
+  },
+
+  playFirst: function() {
+    this.at(0).play();
+  },
+
+  enqueue: function(song) {
+    if (this.length === 1) {
+      this.playFirst();
+    }
+    this.add(song);
+  },
+
+  dequeue: function() {
+    this.shift();
+    if (this.length >= 1) {
+      this.playFirst();
+    }
+  },
+
+  end: function() {
+    this.dequeue();
   }
 
 });
